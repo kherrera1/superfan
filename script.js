ae87899a6758c507d93d5baccc6db06d08b20a0b
kherrera1 <45076685+kherrera1@users.noreply.github.com>
2019-12-17 12:24:52 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// functions for each product
+function changeImage1(){
+	document.getElementById("img1").src = "images/james_brand.jpeg";
+	document.getElementById("img9").src = "images/charles.gif";
+	document.getElementById("img1").classList.remove("hover-effect");
+}
+
+function changeImage2(){
+	document.getElementById("img2").src = "images/annette_brand.png";
+	document.getElementById("img2").classList.remove("hover-effect");
+}
+
+function changeImage3(){
+	document.getElementById("img3").src = "images/fran_brand.jpg";
+	document.getElementById("img3").classList.remove("hover-effect");
+}
+
+function changeImage4(){
+	document.getElementById("img4").src = "images/jeffre_brand.png";
+	document.getElementById("img10").src = "images/starrrr.gif";
+	document.getElementById("img4").classList.remove("hover-effect");
+
+}
+
+function changeImage5(){
+	document.getElementById("img5").src = "images/kim_brand.jpeg";
+	document.getElementById("img5").classList.remove("hover-effect");
+}
+
+function changeImage6(){
+	document.getElementById("img6").src = "images/huda_brand.jpeg";
+	document.getElementById("img6").classList.remove("hover-effect");
+}
+
+function changeImage7(){
+	document.getElementById("img7").src = "images/kylie_brand.jpg";
+	document.getElementById("img7").classList.remove("hover-effect");
+}
+
+
+function changeImage8(){
+	document.getElementById("img8").src = "images/ana_brand.png";
+	document.getElementById("img8").classList.remove("hover-effect");
+}
